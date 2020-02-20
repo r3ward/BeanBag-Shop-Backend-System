2e6904ddecada2b68a57543476cf675eb609e096
dgbarclay <dan@danbarclay.com>
2020-02-20 13:02:37 +0000

    beanbags package

---
+++ beanbags.java
@@ -0,0 +1,19 @@
+public class BeanBag
+{
+  // Attributes for a bean bag:
+  // -the manufacturer
+  // -the bean bag name
+  // -the price
+  // -an ID number (an eight character string holding a positive hexadecimal number)
+  // -the year of manufacture
+  // -the month of manufacture
+  // -optional free text component
+  String manufacturer;
+  String name;
+  Float price;
+  String ID;
+  Int year_manufacture;
+  Int month_manufacture;
+  String text;
+
+}
