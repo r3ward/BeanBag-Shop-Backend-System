dee293c4548363c8faeae924c7716c85a6d2e4f8
dgbarclay <dan@danbarclay.com>
2020-02-21 13:04:24 +0000

    dan fix

--- beanbags.java
+++ beanbags.java
@@ -1,4 +1,4 @@
-public class beanbags
+public class beanbags //may need to start with a capital letter??????
 {
   // Attributes for a bean bag:
   // -the manufacturer
@@ -14,10 +14,42 @@ public class beanbags
   short year;
   byte month;
 
-  public beanBag(int num, String manufacturer, String name,
-  String id, short year, byte month){
-    
+  public beanBag(int initialNum, String initialManufacturer, String initialName,
+  String initialId, short initialYear, byte initialMonth){
+
+    num = initialNum
+    manufacturer = initialManufacturer
+    name = initialName
+    id = initialId
+    year = initialYear
+    month = initialMonth
   }
+  /**
+     * This is a copy constructor. It initializes the fields of the object being created to the same
+     * values as the fields in the object passed as an argument.
+     *
+     * @param beanBagObject The object being copied.
+     */
+
+  public beanBag(beanbags beanBagObject)
+    {
+        if (beanBagObject != null)
+        {
+            manufacturer = beanBagObject.manufacturer;
+            num = beanBagObject.num;
+            name = beanBagObject.name;
+            id = beanBagObject.id;
+            year = beanBagObject.year;
+            month = beanBagObject.month;
+        }
+    }
+
+  public String getManufacturer(){
+    return manufacturer
+  }
+
+  // continue creating to access other attributres of the object.
+  // also create functions to modify the attributes.
 
 
 
