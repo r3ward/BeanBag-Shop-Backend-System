acfc89a878c0b26bb3562b995126bbc38f700dce
dgbarclay <dan@danbarclay.com>
2020-02-21 15:06:41 +0000

    Update beanbags.java

--- beanbags.java
+++ beanbags.java
@@ -1,4 +1,4 @@
-public class beanbags //may need to start with a capital letter??????
+public class BeanBags //may need to start with a capital letter??????
 {
   // Attributes for a bean bag:
   // -the manufacturer
@@ -10,13 +10,13 @@ public class beanbags //may need to start with a capital letter??????
   // -optional free text component
 
   String manufacturer, name, text, id;
-  int num;
+  int num;    // price
   short year;
   byte month;
 
   public beanBag(int initialNum, String initialManufacturer, String initialName,
   String initialId, short initialYear, byte initialMonth){
-
+    
     num = initialNum
     manufacturer = initialManufacturer
     name = initialName
@@ -31,7 +31,7 @@ public class beanbags //may need to start with a capital letter??????
      * @param beanBagObject The object being copied.
      */
 
-  public beanBag(beanbags beanBagObject)
+  public beanBag(BeanBags beanBagObject)
     {
         if (beanBagObject != null)
         {
@@ -48,9 +48,28 @@ public class beanbags //may need to start with a capital letter??????
     return manufacturer
   }
 
-  // continue creating to access other attributres of the object.
-  // also create functions to modify the attributes.
+  public int getNum():{
+    return num
+  }
 
+  public String getName(){
+    return name
+  }
 
+  public String getId(){
+    return id
+  }
 
+  public short getYear(){
+    return year
+  }
+
+  public byte getMonth(){
+    return month
+  }
+
+  public void setPrice(int newNum) // MAY NOT BE NEEDED
+    {
+        num = newNum;
+    }
 }
