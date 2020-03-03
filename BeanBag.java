df9252ce3f50bd204b126fc011bcc449482fb4e8
dgbarclay <dan@danbarclay.com>
2020-03-03 17:01:57 +0000

    WORKING

--- BeanBag.java
+++ BeanBag.java
@@ -15,7 +15,7 @@ public class BeanBag //may need to start with a capital letter??????
       // Integer.toHexString(value);
       // Following statement would create an object testBag
       BeanBag testBag = new BeanBag(1200, "Nike", "Specialé", "1234", 20, 01);
-      // BeanBag[] beanBagArray = new BeanBag[];
+      BeanBag[] beanBagArray = new BeanBag[6];
 
       manufacturer = testBag.getManufacturer();
       name = testBag.getName();
@@ -31,7 +31,7 @@ public class BeanBag //may need to start with a capital letter??????
       System.out.println(Integer.toString(year));
       System.out.println(Integer.toString(month));
 
-}
+    }
 
   // Attributes for a bean bag:
   // -the manufacturer
@@ -47,16 +47,16 @@ public class BeanBag //may need to start with a capital letter??????
   int year;
   int month;
   //
-  // public BeanBag(int initialNum, String initialManufacturer, String initialName,
-  // String initialId, int initialYear, int initialMonth){
-  //
-  //   num = initialNum;
-  //   manufacturer = initialManufacturer;
-  //   name = initialName;
-  //   id = initialId;
-  //   year = initialYear;
-  //   month = initialMonth;
-  // }
+  public BeanBag(int initialNum, String initialManufacturer, String initialName,
+  String initialId, int initialYear, int initialMonth){
+  
+    num = initialNum;
+    manufacturer = initialManufacturer;
+    name = initialName;
+    id = initialId;
+    year = initialYear;
+    month = initialMonth;
+  }
   /**
      * This is a copy constructor. It initializes the fields of the object being created to the same
      * values as the fields in the object passed as an argument.
@@ -104,18 +104,19 @@ public class BeanBag //may need to start with a capital letter??????
     return month;
   }
 
+  // public boolean getReservationStatus(){
+  //   return reserved;
+  // }
+
   public void setPrice(int newNum) // MAY NOT BE NEEDED
-    {
-        num = newNum;
-    }
+  {
+    num = newNum;
+  }
+  
 
   public BeanBag copy(){
-        return new BeanBag(this);
-    }
-
-  public boolean getReservationStatus(){
-        return reserved;
-    }
+    return new BeanBag(this);
+  }
 
 
 }
